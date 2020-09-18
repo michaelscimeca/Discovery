4ba19c59cf94b49209de8adc774b9583530a5bd8
Mikeyscimeca <mikeys@rsq.com>
2020-09-18 11:29:43 -0500

    muse

--- BMC/js/modules/mouse.js
+++ BMC/js/modules/mouse.js
@@ -1,45 +1,49 @@
 'use strict';
-// import gsap from 'gsap';
+import { TweenLite, Elastic } from 'gsap';
 module.exports = function () {
   const cursor = document.querySelector('#cursor');
   const body = document.querySelector('body');
-  // const detection = document.querySelector('#detection');
-  const elements = document.querySelectorAll('[data-grab="link"]');
-
+  const items = document.querySelectorAll('[data-grab="link"]');
   class Mouse {
-    constructor () {
-      this.runRAF = false;
+    constructor (cursor, items) {
       this.mX = 0;
       this.mY = 0;
+      this.mcX = 0;
+      this.mcY = 0;
+      this.size = cursor.getBoundingClientRect().width;
+      this.recenter = (c) => {
+        return c - (this.size / 2);
+      };
+      this.runRAF = false;
       this.distance = 0;
       this.domData = [];
       this.build = () => {
-        elements.forEach((element, i) => {
+        items.forEach((element, i) => {
           this.domData.push({
-            info: {
-              element: element,
-              index: i,
-              proximity: element.dataset.proximity,
-              type: element.dataset.grab,
-              height: element.getBoundingClientRect().height,
-              width: element.getBoundingClientRect().width,
-              x: element.getBoundingClientRect().x,
-              y: element.getBoundingClientRect().y
-            }
+            element: element,
+            index: i,
+            proximity: element.dataset.proximity,
+            type: element.dataset.grab,
+            height: element.getBoundingClientRect().height,
+            width: element.getBoundingClientRect().width,
+            x: element.getBoundingClientRect().x,
+            y: element.getBoundingClientRect().y,
+            cX: element.getBoundingClientRect().x + (element.getBoundingClientRect().width / 2) - (this.size / 2),
+            cY: element.getBoundingClientRect().y + (element.getBoundingClientRect().height / 2)
           });
         });
       };
       this.resize = () => {
         this.build();
       };
-      this.calculateDistance = (element, mouseX, mouseY) => {
-        const e = element.getBoundingClientRect();
+      this.calculateDistance = (element, x, y) => {
+        const elm = element.getBoundingClientRect();
         return Math.floor(
           Math.sqrt(
-            Math.pow(mouseX - (e.left + (e.width / 2)), 2) +
-            Math.pow(mouseY - (e.top + (e.height / 2)), 2)
+            Math.pow(x - (elm.left + (elm.width / 2)), 2) +
+            Math.pow(y - (elm.top + (elm.height / 2)), 2)
           )
-        ) - Math.round(e.width / 2);
+        ) - Math.round(elm.width / 2);
       };
       this.cursorIn = (e) => {
         cursor.classList.add('active');
@@ -47,23 +51,47 @@ module.exports = function () {
       this.cursorOut = (e) => {
         cursor.classList.remove('active');
       };
-      this.update = (e) => {
-        if (!this.runRAF) return;
-        cursor.style.transform = `translate(${e.x}px, ${e.y}px)`;
-        requestAnimationFrame(this.update);
-      };
       this.cursorMovement = (e) => {
-        this.runRAF = true;
-        setTimeout(() => { this.runRAF = false; }, 300);
-        this.update(e);
         this.mX = e.pageX;
         this.mY = e.pageY;
-        this.domData.forEach((elm, i) => {
-          this.distance = this.calculateDistance(elm.info.element, this.mX, this.mY);
-          if (this.distance < elm.info.proximity) {
-            cursor.classList.add('triggered');
+        this.mcX = this.recenter(this.mX);
+        this.mcY = this.recenter(this.mY);
+        for (let i = 0; i < this.domData.length; i++) {
+          this.distance = this.calculateDistance(this.domData[i].element, this.mX, this.mY);
+          if (this.distance < this.domData[i].proximity) {
+            // TweenLite.to(cursor, {
+            //      duration: 0.5,
+            //      x: this.domData[i].cX,
+            //      y: this.domData[i].cY
+            //    });
+            cursor.style.transform = `translate(${this.domData[i].cX}px, ${this.recenter(this.domData[i].cY)}px)`;
+          } else {
+            cursor.style.transform = `translate(${this.mcX}px, ${this.mcY}px)`;
           }
-        });
+        }
+        this.update();
+        this.runRAF = true;
+        setTimeout(() => { this.runRAF = false; }, 300);
+      };
+      this.update = () => {
+        if (!this.runRAF) return;
+        //
+        // for (let i = 0; i < this.domData.length; i++) {
+        //   this.distance = this.calculateDistance(this.domData[i].element, this.mX, this.mY);
+        //   if (this.distance < this.domData[i].proximity) {
+        //     console.log(this.domData[i].cX, this.domData[i].element)
+        //     // TweenLite.to(cursor, {
+        //     //   duration: 0.5,
+        //     //   x: this.domData[i].cX,
+        //     //   y: this.domData[i].cY
+        //     // });
+        //
+        //     cursor.style.transform = `translate(${this.domData[i].cX}px, ${this.recenter(this.domData[i].cY)}px)`;
+        //   } else {
+        //     cursor.style.transform = `translate(${this.mcX}px, ${this.mcY}px)`;
+        //   }
+        // }
+        requestAnimationFrame(this.update);
       };
       this.init = () => {
         this.build();
@@ -75,12 +103,6 @@ module.exports = function () {
     }
   }
 
-  const mouse = new Mouse(elements, 100);
+  const mouse = new Mouse(cursor, items);
   mouse.init();
-
-
-
-  // window.addEventListener('mousemove', (e) => cursorMovement(e));
-  // body.addEventListener('mouseenter', (e) => cursorIn(e));
-  // body.addEventListener('mouseleave', (e) => cursorOut(e));
 };
