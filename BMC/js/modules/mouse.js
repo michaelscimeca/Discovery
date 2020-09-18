370f05ebe0f16864ba0b598d1fa574691d45a6e1
Mikeyscimeca <mikeys@rsq.com>
2020-09-18 15:21:27 -0500

    updated mouse object

--- BMC/js/modules/mouse.js
+++ BMC/js/modules/mouse.js
@@ -1,11 +1,12 @@
 'use strict';
-import { TweenLite, Elastic } from 'gsap';
+import { TweenLite, TweenMax, Elastic } from 'gsap';
 module.exports = function () {
   const cursor = document.querySelector('#cursor');
   const body = document.querySelector('body');
   const items = document.querySelectorAll('[data-grab="link"]');
   class Mouse {
     constructor (cursor, items) {
+      this.freeze = false;
       this.mX = 0;
       this.mY = 0;
       this.mcX = 0;
@@ -29,7 +30,7 @@ module.exports = function () {
             x: element.getBoundingClientRect().x,
             y: element.getBoundingClientRect().y,
             cX: element.getBoundingClientRect().x + (element.getBoundingClientRect().width / 2) - (this.size / 2),
-            cY: element.getBoundingClientRect().y + (element.getBoundingClientRect().height / 2)
+            cY: element.getBoundingClientRect().y + (element.getBoundingClientRect().height / 2) - (this.size / 2)
           });
         });
       };
@@ -56,19 +57,36 @@ module.exports = function () {
         this.mY = e.pageY;
         this.mcX = this.recenter(this.mX);
         this.mcY = this.recenter(this.mY);
-        for (let i = 0; i < this.domData.length; i++) {
-          this.distance = this.calculateDistance(this.domData[i].element, this.mX, this.mY);
-          if (this.distance < this.domData[i].proximity) {
-            // TweenLite.to(cursor, {
-            //      duration: 0.5,
-            //      x: this.domData[i].cX,
-            //      y: this.domData[i].cY
-            //    });
-            cursor.style.transform = `translate(${this.domData[i].cX}px, ${this.recenter(this.domData[i].cY)}px)`;
+
+        this.domData.forEach((item, i) => {
+          this.distance = this.calculateDistance(item.element, this.mX, this.mY);
+          if (this.distance <= item.proximity) {
+            this.freeze = true;
+            TweenMax.to(cursor, 0.5, {
+              y: item.cY,
+              x: item.cX,
+              onComplete () {
+                this.freeze = false;
+                console.log(this.freeze)
+              }
+            });
           } else {
-            cursor.style.transform = `translate(${this.mcX}px, ${this.mcY}px)`;
+            // console.log('false')
+            console.log(this.freeze,'d')
+            if (!this.freeze) {
+              TweenMax.to(cursor, 0.5, { y: this.mcY, x: this.mcX });
+            }
           }
-        }
+        });
+
+        // for (let i = 0; i < this.domData.length; i++) {
+        //   // if (this.distance < this.domData[i].proximity) {
+        //   //    TweenMax.to(cursor, 0.5, {y: this.domData[i].cY, x: this.domData[i].cX});
+        //   // } else {
+        //   //    TweenMax.to(cursor, 0.5, {y: this.mcY, x: this.mcX});
+        //   //   // cursor.style.transform = `translate(${this.mcX}px, ${this.mcY}px)`;
+        //   // }
+        // }
         this.update();
         this.runRAF = true;
         setTimeout(() => { this.runRAF = false; }, 300);
